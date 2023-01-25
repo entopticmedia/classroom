2dc093b6ca50120f5f69f24d87e71092b8d33e1c
Tim Korjakow <you@example.com>
2023-01-25 11:37:59 +0100

    Added one more domain to allowed origins

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@ jupyter kernelgateway \
     --KernelGatewayApp.api='kernel_gateway.notebook_http' \
     --KernelGatewayApp.seed_uri='./classroom_backend.ipynb' \
     --KernelGatewayApp.prespawn_count=1 \
-    --KernelGatewayApp.port=8889
+    --KernelGatewayApp.port=8889 \
+    --KernelGatewayApp.allow_origin='https://(entoptic.media|designresearch.works)'
