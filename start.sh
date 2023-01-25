907b3961ca6b21370f67cf90498564a3ecbfae26
Entoptic User <input@entoptic.media>
2023-01-25 10:39:07 +0000

    Local changes

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 jupyter kernelgateway \
     --KernelGatewayApp.api='kernel_gateway.notebook_http' \
     --KernelGatewayApp.seed_uri='./classroom_backend.ipynb' \
-    --KernelGatewayApp.prespawn_count=1 \
-    --KernelGatewayApp.port=8889
+    --KernelGatewayApp.prespawn_count=5 \
+    --KernelGatewayApp.port=8891
